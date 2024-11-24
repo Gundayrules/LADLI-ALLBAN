2179e89cfe5abde41eafccc9140975a4e871456e
Atul tiwari <162774395+Gundayrules@users.noreply.github.com>
2024-11-24 14:36:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=22981583
-    API_HASH="c02ca0834d5d92ffb31fea2b50060195"
-    TOKEN="7156025226:AAFaTvFJwfkHLGcP2JZqY6GbfnUfajkQY5o"
+    API_ID=24038950
+    API_HASH="34f981f08292fe6892ed34f6c38fe406"
+    TOKEN="7665576624:AAE7XJxwFJDKzMCY6Hvqzq0KGHAHSnjms3g"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6752444356").split(" "))
-    START_IMG="https://te.legra.ph/file/735920504b44b3565242a.jpg"
-    BOT_ID=7156025226
+    START_IMG="https://files.catbox.moe/uoodut.jpg"
+    BOT_ID=7879203007
     BOT_USERNAME="SOMEONE47BOT"
     BOT_NAME="SOMEONE47BOT"
